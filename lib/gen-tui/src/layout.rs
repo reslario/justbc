11af18e3dcb3ada19ed157ba8bb7cdf40bb4d0c1
reslario <reslario.code@gmail.com>
2020-11-01 14:45:50 +0100

    Add more RectExt methods

--- lib/gen-tui/src/layout.rs
+++ lib/gen-tui/src/layout.rs
@@ -1,8 +1,31 @@
-pub trait RectExt: Sized {
+pub trait RectExt: Sized + Copy {
     fn shrink_left(self, amount: u16) -> Self;
     fn shrink_right(self, amount: u16) -> Self;
     fn shrink_top(self, amount: u16) -> Self;
     fn shrink_bottom(self, amount: u16) -> Self;
+    
+    fn grow_left(self, amount: u16) -> Self;
+    fn grow_right(self, amount: u16) -> Self;
+    fn grow_top(self, amount: u16) -> Self;
+    fn grow_bottom(self, amount: u16) -> Self;
+
+    fn from_left(self, width: u16) -> Self;
+    fn from_right(self, width: u16) -> Self;
+    fn from_top(self, height: u16) -> Self;
+    fn from_bottom(self, height: u16) -> Self;
+
+    #[inline]
+    fn split_x(self, x: u16) -> (Self, Self) {
+        (self.from_left(x), self.shrink_left(x))
+    }
+
+    #[inline]
+    fn split_y(self, y: u16) -> (Self, Self) {
+        (self.from_top(y), self.shrink_top(y))
+    }
+
+    fn split_ratio_x(self, ratio: f32) -> (Self, Self);
+    fn split_ratio_y(self, ratio: f32) -> (Self, Self);
 }
 
 impl RectExt for tui::layout::Rect {
@@ -35,11 +58,82 @@ impl RectExt for tui::layout::Rect {
             ..self
         }
     }
+
+    #[inline]
+    fn grow_left(self, amount: u16) -> Self {
+        let (x, width) = grow_adjust(self.x, self.width, amount);
+
+        Self { x, width, ..self }
+    }
+
+    #[inline]
+    fn grow_right(self, amount: u16) -> Self {
+        Self {
+            width: self.width.saturating_add(amount),
+            ..self
+        }
+    }
+
+    #[inline]
+    fn grow_top(self, amount: u16) -> Self {
+        let (y, height) = grow_adjust(self.y, self.height, amount);
+
+        Self { y, height, ..self }
+    }
+    
+    #[inline]
+    fn grow_bottom(self, amount: u16) -> Self {
+        Self {
+            height: self.height.saturating_add(amount),
+            ..self
+        }
+    }
+
+    #[inline]
+    fn from_left(self, width: u16) -> Self {
+        self.shrink_right(self.width.saturating_sub(width))
+    }
+
+    #[inline]
+    fn from_right(self, width: u16) -> Self {
+        self.shrink_left(self.width.saturating_sub(width))
+    }
+
+    #[inline]
+    fn from_top(self, height: u16) -> Self {
+        self.shrink_bottom(self.height.saturating_sub(height))
+    }
+
+    #[inline]
+    fn from_bottom(self, height: u16) -> Self {
+        self.shrink_top(self.height.saturating_sub(height))
+    }
+
+    #[inline]
+    fn split_ratio_x(self, ratio: f32) -> (Self, Self) {
+        let x = f32::from(self.width) * ratio;
+
+        self.split_x(x.round() as u16)
+    }
+
+    #[inline]
+    fn split_ratio_y(self, ratio: f32) -> (Self, Self) {
+        let y = f32::from(self.height) * ratio;
+
+        self.split_y(y.round() as u16)
+    }
 }
 
 fn shrink_adjust(coord: u16, size: u16, amount: u16) -> (u16, u16) {
     (
         coord.saturating_add(amount),
         size.saturating_sub(amount)
+    )
+}
+
+fn grow_adjust(coord: u16, size: u16, amount: u16) -> (u16, u16) {
+    (
+        coord.saturating_sub(amount),
+        size.saturating_add(amount)
     )
 }
