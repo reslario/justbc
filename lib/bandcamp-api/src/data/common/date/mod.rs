c126296b9d95ef0c3df9f37857514fddcd8c5372
reslario <reslario.code@gmail.com>
2020-11-04 10:45:52 +0100

    Make Date Copy & add formatting methods

--- lib/bandcamp-api/src/data/common/date/mod.rs
+++ lib/bandcamp-api/src/data/common/date/mod.rs
@@ -5,14 +5,46 @@ use std::{
     num::NonZeroU8
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Date {
     pub day: NonZeroU8,
     pub month: Month,
     pub year: u16
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+impl Date {
+    pub fn fmt_short(self) -> impl fmt::Display {
+        FmtDate {
+            date: self,
+            month_str: Month::short
+        }
+    }
+
+    pub fn fmt_long(self) -> impl fmt::Display {
+        FmtDate {
+            date: self,
+            month_str: Month::long
+        }
+    }
+}
+
+struct FmtDate {
+    date: Date,
+    month_str: fn(Month) -> &'static str
+}
+
+impl fmt::Display for FmtDate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f, "{} {} {}",
+            self.date.day,
+            (self.month_str)(self.date.month),
+            self.date.year
+        )
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Month {
     January = 1,
     February,
@@ -50,7 +82,7 @@ impl Month {
         Month::ALL.iter().cloned()
     }
 
-    pub fn long(&self) -> &'static str {
+    pub fn long(self) -> &'static str {
         macro_rules! long {
             ( $($month:ident),+ ) => {
                 match self {
@@ -79,11 +111,11 @@ impl Month {
         )
     }
 
-    pub fn short(&self) -> &'static str {
+    pub fn short(self) -> &'static str {
         &self.long()[..Month::SHORT_LEN]
     }
 
-    fn matches_str(&self, s: &str) -> bool {
+    fn matches_str(self, s: &str) -> bool {
         if let Some(rest) = s.strip_prefix(self.short()) {
             rest.is_empty() || rest == &self.long()[Month::SHORT_LEN..]
         } else {
