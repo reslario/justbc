811cedb946ebd1f2f936a35d92de7282238633c3
reslario <reslario.code@gmail.com>
2020-11-27 14:37:46 +0100

    Make the spinner widget's animation speed agnostic to the update rate

--- lib/gen-tui/src/widgets/spinner.rs
+++ lib/gen-tui/src/widgets/spinner.rs
@@ -1,6 +1,9 @@
 use {
-    std::iter,
     builder::builder_method,
+    std::{
+        iter,
+        time::{Instant, Duration}
+    },
     tui::{
         style::Style,
         layout::Rect,
@@ -9,14 +12,27 @@ use {
     }
 };
 
-#[derive(Default)]
 pub struct Spinner {
-    style: Style
+    style: Style,
+    interval: Duration
 }
 
 impl Spinner {
     builder_method! {
         pub style: Style
+    }
+
+    pub fn per_rotation(self, duration: Duration) -> Self {
+        Self { interval: duration / SYMBOLS.len() as u32, ..self }
+    }
+}
+
+impl Default for Spinner {
+    fn default() -> Self {
+        Spinner {
+            style: <_>::default(),
+            interval: <_>::default()
+        }.per_rotation(Duration::from_millis(500))
     }
 }
 
@@ -27,8 +43,40 @@ type Iter = iter::Cycle<iter::Zip<
     Symbols,
     iter::Chain<iter::Skip<Symbols>, std::option::IntoIter<Symbol>>>
 >;
+
 pub struct SpinnerState {
-    symbols: Iter
+    symbols: Iter,
+    last_symbols: (Symbol, Symbol),
+    last_step: Instant
+}
+
+impl SpinnerState {
+    fn steps(&mut self, interval: Duration) -> usize {
+        let steps = self
+            .last_step
+            .elapsed()
+            .as_millis()
+            / interval.as_millis();
+
+        if steps > 0 {
+            self.last_step = Instant::now()
+        }
+
+        steps as _
+    }
+}
+
+impl Default for SpinnerState {
+    fn default() -> Self {
+        let mut symbols = symbol_pairs();
+        let last_symbols = symbols.next().unwrap();
+        
+        SpinnerState {
+            symbols,
+            last_symbols,
+            last_step: Instant::now(),
+        }
+    }
 }
 
 const SYMBOLS: &[Symbol] = &[
@@ -40,16 +88,12 @@ const SYMBOLS: &[Symbol] = &[
     ([0, 0], '╭'),
 ];
 
-impl Default for SpinnerState {
-    fn default() -> Self {
-        SpinnerState {
-            symbols: symbols()
-                .zip(symbols()
-                    .skip(1)
-                    .chain(symbols().next())
-                ).cycle()
-        }
-    }
+fn symbol_pairs() -> Iter {
+    symbols()
+        .zip(symbols()
+            .skip(1)
+            .chain(symbols().next())
+        ).cycle()
 }
 
 fn symbols() -> Symbols {
@@ -69,14 +113,24 @@ impl StatefulWidget for Spinner {
 
         let mut cells = [[' '; WIDTH as _]; HEIGHT as _];
 
-        let (a, b) = state.symbols.next().unwrap();
+        let steps = state.steps(self.interval);
+
+        let (a, b) = match steps {
+            0 => state.last_symbols,
+            n => state
+                .symbols
+                .nth(n - 1)
+                .unwrap()
+        };
+
+        state.last_symbols = (a, b);
 
         insert(a, &mut cells);
         insert(b, &mut cells);
 
         for (y, row) in cells.iter().enumerate() {
             for (x, sym) in row.iter().enumerate() {
-                buf.get_mut(x as _, y as _)
+                buf.get_mut(area.x + x as u16, area.y + y as u16)
                     .set_char(*sym)
                     .set_style(self.style);
             }
