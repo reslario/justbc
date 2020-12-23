5b71f36109b26d93fc273ac6d18bf5a18c6aa904
reslario <reslario.code@gmail.com>
2020-12-23 16:43:54 +0100

    Refactor play error handling to use snafu everywhere

--- lib/play/src/lib.rs
+++ lib/play/src/lib.rs
@@ -10,16 +10,15 @@ use {
     snafu::{Snafu, ResultExt},
     std::{
         fmt,
-        error,
         time::Duration,
     }
 };
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("Error playing sound: {}", source))]
+    #[snafu(context(false), display("Error playing sound: {}", source))]
     Play { source: PlayError },
-    #[snafu(display("Error opening output stream: {}", source))]
+    #[snafu(context(false), display("Error opening output stream: {}", source))]
     Stream { source: StreamError }
 }
 
@@ -66,10 +65,8 @@ where
     
     fn set_source(&mut self, source: S) -> Result<(), Error> {
         let sink = self
-            .init_handle()
-            .context(Stream)?
-            .new_sink()
-            .context(Play)?;
+            .init_handle()?
+            .new_sink()?;
 
         sink.set_volume(self.volume);
         
@@ -129,14 +126,12 @@ where
     /// if it has changed.
     pub fn update_device(&mut self) -> Result<(), UpdateDeviceError> {
         let handle = Handle::new()
-            .context(Stream)
-            .map_err(UpdateDeviceError::General)?;
+            .map_err(Error::from)?;
 
         if let Some(track) = self.current.as_mut() {
             let sink = handle
                 .new_sink()
-                .context(Play)
-                .map_err(UpdateDeviceError::General)?;
+                .map_err(Error::from)?;
 
             track.set_sink(sink)
                 .map_err(|_| UpdateDeviceError::Resume)?
@@ -166,23 +161,26 @@ where
 
     /// Seeks to the specified duration in the current track, if one exists.
     pub fn seek(&mut self, duration: Duration) -> Result<(), SeekError<S::Error>>
-    where S: seek::SeekableSource {
+    where 
+        S: seek::SeekableSource,
+        S::Error: fmt::Display + snafu::Error
+    {
         let paused = self.is_paused();
 
         if let Some(track) = self.current.take() {
             let mut source = track
                 .into_source()
-                .map_err(|_| SeekError::Resume)?;
+                .map_err(|_| SeekError::ResumePlayback)?;
 
             let elapsed = source
                 .seek(duration)
-                .map_err(SeekError::Seek)?;
+                .context(Seek)?;
 
             if paused {
                 self.set_source(source)
             } else {
                 self.play(source)
-            }.map_err(SeekError::General)?;
+            }?;
 
             self.mut_track(|track| track.set_elapsed(elapsed));
         }
@@ -191,49 +189,29 @@ where
     }
 }
 
-/// Errors that can occur when seeking.
-#[derive(Debug)]
-pub enum SeekError<E> {
-    /// An error occured when seeking.
-    Seek(E),
-    /// A general playback error occured.
-    General(Error),
-    /// Resuming playback was not possible.
-    Resume
-}
-
 const FAILED_RESUME: &str = "failed to resume playback";
 
-impl <E: fmt::Display> fmt::Display for SeekError<E> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            SeekError::Seek(e) => e.fmt(f),
-            SeekError::General(e) => e.fmt(f),
-            SeekError::Resume => FAILED_RESUME.fmt(f)
-        }
-    }
+/// Errors that can occur when seeking.
+#[derive(Debug, Snafu)]
+pub enum SeekError<E: fmt::Display + snafu::Error + 'static> {
+    /// An error occured when seeking.
+    #[snafu(display("failed to seek: {}", source))]
+    Seek { source: E },
+    /// A general playback error occured.
+    #[snafu(context(false), display("playback error when seeking: {}", source))]
+    General { source: Error },
+    /// Resuming playback was not possible.
+    #[snafu(display("{}", FAILED_RESUME))]
+    ResumePlayback
 }
-
-impl <E: error::Error> error::Error for SeekError<E> {}
 
 /// Errors that can occur when updating the device.
-#[derive(Debug)]
+#[derive(Debug, Snafu)]
 pub enum UpdateDeviceError {
     /// A general playback error occured.
-    General(Error),
+    #[snafu(context(false), display("{}", source))]
+    General { source: Error },
     /// Resuming playback was not possible.
+    #[snafu(display("{}", FAILED_RESUME))]
     Resume
 }
-
-impl fmt::Display for UpdateDeviceError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "failed to update device: ")?;
-
-        match self {
-            UpdateDeviceError::General(e) => e.fmt(f),
-            UpdateDeviceError::Resume => FAILED_RESUME.fmt(f)
-        }
-    }
-}
-
-impl error::Error for UpdateDeviceError {}
